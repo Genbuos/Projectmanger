b8084c7b45491710d126a398525c510494db56f4
Genbuos <swiftypython18@gmail.com>
2023-11-30 01:15:00 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,29 @@
+project = "What is the project?"
+
+language = "What is the language?"
+
+#this is how you comment in py!
+
+#input() is a function built in to python.
+"""
+this is a multiline comment.
+
+:input() is used to take in input from the console. or "user"
+
+
+im gonna use this to build a to do app to help me 
+manage any projects that i may be working on !
+
+"""
+todo1P = input(project)
+todo1L = input(language)
+todo2P = input(project)
+todo2L = input(language)
+todo3P = input(project)
+todo3L = input(language)
+
+
+
+todos = {todo1P:todo1L, todo2P:todo2L, todo3P:todo3L}
+print(todos)
+print(type(todos))
